64050b2a967cf797d59d6c6e948241946f7c4a72
kurnakovv <maksk210@gmail.com>
2026-06-24 15:07:43 +0900

    Hide icons for Enable/Disable btn's (#4)

--- BiakVisualStudioExtension/BiakVisualStudioExtensionPackage.cs
+++ BiakVisualStudioExtension/BiakVisualStudioExtensionPackage.cs
@@ -52,6 +52,7 @@ public sealed class BiakVisualStudioExtensionPackage : AsyncPackage
         // When initialized asynchronously, the current thread may be a background thread at this point.
         // Do any initialization that requires the UI thread after switching to the UI thread.
         await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        await BiakCommand.InitializeAsync(this);
         await EnableCommand.InitializeAsync(this);
         await DisableCommand.InitializeAsync(this);
     }
